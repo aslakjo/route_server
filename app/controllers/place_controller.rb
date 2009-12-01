e5a3cb355880c8876d8dfd8505aa4d7fd583736c
Aslak Johannessen <aslak@x200.(none)>
2009-12-01 23:08:19 +0100

    adding some feature click on map == a new marker, loading the map vil give all markers created, in first stages

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,10 +1,14 @@
 class PlaceController < ApplicationController
   def new
-    p params
     place = JSON.parse(params[:place])
-    point = place[:point]
+    point = Point.create!(place["point"])
     
     Place.create!(place.except(:point).merge(:point => point))
     render :text => '{status: "ok"}', :status => 200
+  end
+
+  def index
+    @places = Place.all
+    render :json => @places.to_json(:include => :point)
   end
 end
--- spec/controllers/place_controller_spec.rb
+++ spec/controllers/place_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe PlaceController do
-
-
   it "should be able to add" do
     json= '{ "title": "title", "description": "description", "point": { "lat": "10.0000" , "lng": "60.60"}}'
     post :new, :place => json
@@ -15,4 +13,16 @@ describe PlaceController do
     place.point.should == point
   end
 
+  it "should give out all its places" do
+    @valid_attr = {:title => "title", :description => "desc", :point => Point.create!({:lat=>10.0, :lng=> 60.0})}
+    Place.create!(@valid_attr)
+    Place.create!(@valid_attr)
+
+    get :index
+    
+    response.body.should =~ /title/
+    response.body.should =~ /desc/
+    response.body.should =~ /10.0/
+    response.body.should =~ /60.0/
+  end
 end
